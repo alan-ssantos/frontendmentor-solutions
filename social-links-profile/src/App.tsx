f310692d9475b690a75c1c18659d0a0102c2e176
Alan Santos <alansousa@outlook.com>
2026-02-09 00:54:00 -0300

    refactor: update `App` to get username from `params` and use `Profile` components

--- social-links-profile/src/App.tsx
+++ social-links-profile/src/App.tsx
@@ -1,6 +1,15 @@
-import { useState } from "react";
 import useSWR from "swr";
+import { useParams } from "react-router";
 import type { ApiUserInterface, ApiErrorInterface } from "./types/api";
+
+import Profile from "./components/Profile/Profile";
+import ProfileHeader from "./components/Profile/ProfileHeader";
+import ProfilePicture from "./components/Profile/ProfilePicture";
+import ProfileName from "./components/Profile/ProfileName";
+import ProfileLocation from "./components/Profile/ProfileLocation";
+import ProfileDescription from "./components/Profile/ProfileDescription";
+import ProfileNavLinks from "./components/Profile/ProfileNavLinks";
+import ProfileLink from "./components/Profile/ProfileLink";
 
 const API_URL = import.meta.env.VITE_GITHUB_API_URL;
 
@@ -11,6 +20,7 @@ async function fetcher(key: string): Promise<ApiUserInterface> {
     if (response.status === 404) {
       throw new Error("Usuário não encontrado!");
     }
+
     if (response.status === 403) {
       throw new Error("Limite de requisições atingido!");
     }
@@ -23,52 +33,35 @@ async function fetcher(key: string): Promise<ApiUserInterface> {
 }
 
 function App() {
-  const [username, setUsername] = useState<string>("alan-ssantos");
-  const { isLoading, data, error } = useSWR(`${API_URL}${username}`, fetcher, {
+  const { username } = useParams();
+
+  const { isLoading, data, error } = useSWR(`${API_URL}/${username}`, fetcher, {
     revalidateOnFocus: false,
   });
-  const [inputValue, setInputValue] = useState<string>("");
 
   if (!data && isLoading) {
     return <div>Carregando informações...</div>;
   }
 
-  function handleFormSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    const trimmedInputValue = inputValue.trim();
-
-    if (trimmedInputValue) {
-      setUsername(trimmedInputValue);
-      setInputValue("");
-    }
-  }
-
   return (
-    <>
-      <h1>Hello World!</h1>
+    <main className="wrapper">
       {error && <div>{error.message}</div>}
 
       {data && (
-        <>
-          <img src={data.avatar_url} alt="" />
-          <p>My name is {data.name}.</p>
-        </>
+        <Profile>
+          <ProfileHeader>
+            <ProfilePicture src={data.avatar_url} alt={`Foto de perfil de ${data.name}`} />
+            <ProfileName>{data.name}</ProfileName>
+            <ProfileLocation>{data.location}</ProfileLocation>
+          </ProfileHeader>
+          <ProfileDescription>{data.bio}</ProfileDescription>
+          <ProfileNavLinks>
+            <ProfileLink href={data.html_url}>GitHub</ProfileLink>
+            {data.blog && <ProfileLink href={data.blog}>Blog / Portfólio</ProfileLink>}
+          </ProfileNavLinks>
+        </Profile>
       )}
-
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          onChange={(e) => setInputValue(e.target.value.toLowerCase())}
-          value={inputValue}
-          placeholder="Search by GitHub username"
-          aria-label="GitHub username"
-          disabled={isLoading}
-        />
-        <button type="submit" disabled={isLoading}>
-          Buscar perfil
-        </button>
-      </form>
-    </>
+    </main>
   );
 }
 
