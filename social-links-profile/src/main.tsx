574d78e012ce5bd3f3d4c4ec528e87b30331b02a
Alan Santos <alansousa@outlook.com>
2026-02-09 00:29:04 -0300

    refactor: implements Routes in `main.tsx` with `MockProfile` as index route

--- social-links-profile/src/main.tsx
+++ social-links-profile/src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MockProfile from "./MockProfile.tsx";
+
+import "./assets/styles/reset.css";
+import "@fontsource-variable/inter";
+import "./assets/styles/main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<MockProfile />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
